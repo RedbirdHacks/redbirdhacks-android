3f99c2339e98425a75c9c3dee7be2b4b79763c28
Andrew Erickson <andrew.erickson.tbmq@statefarm.com>
2014-10-16 21:41:59 -0500

    -- Trivial change

--- src/org/redbird/hacks/MainActivity.java
+++ src/org/redbird/hacks/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends FragmentActivity
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
-
+  
 		super.onCreate(savedInstanceState);
 			setContentView(R.layout.activity_main);
 
